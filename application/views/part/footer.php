3a2e2c7dfe8ce8f068d1c36fa862cccd20c855ff
Javiera Rahmadhany <kazuha004@gmail.com>
2020-10-26 12:42:14 +0800

    Add plugin for input file

--- application/views/part/footer.php
+++ application/views/part/footer.php
@@ -24,6 +24,8 @@
 <script src="<?=base_url('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js');?>"></script>
 <!-- SweetAlert2 -->
 <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js');?>"></script>
+<!-- bs-custom-file-input -->
+<script src="<?=base_url('assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js');?>"></script>
 <!-- AdminLTE App -->
 <script src="<?=base_url('assets/dist/js/adminlte.min.js');?>"></script>
 <!-- AdminLTE for demo purposes -->
